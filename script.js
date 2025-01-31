586c7e19f840f4a738df740d2a251aaba93b3ac0
Adam H <183345857+adamhaouam@users.noreply.github.com>
2025-01-31 14:24:03 +1100

    ADD event listener to change colour of div
    Change div colour on mouseover event

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const container = document.querySelector(".container");
 
 
+
+
 for (let i = 0; i < 16; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.id
     container.appendChild(square);
+}
+
+const squares = document.querySelectorAll(".square")
+
+squares.forEach((square) => {
+    square.addEventListener("mouseenter", (e) => {
+        
+        shade(e.target);
+        
+    })
+});
+
+
+function shade(box) {
+    //square.classList.add("coloured");
+    box.style.backgroundColor = "green";
+    console.log(box);
 }
